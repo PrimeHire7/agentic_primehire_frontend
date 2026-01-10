0a1d7f289440ebef99287d6fb24cf9f255f1db40
Naresh Tinnaluri <chockynaresh18@gmail.com>
2026-01-10 13:38:28 +0530

    updated mail mind

--- src/interview/InterviewMode.jsx
+++ src/interview/InterviewMode.jsx
@@ -211,12 +211,12 @@ export default function InterviewMode() {
             const d = await r.json();
 
             // navigate("/certificatedata", { state: d });
-            navigate("/certificatedata", {
-                state: {
-                    ...d,
-                    anomaly_counts: anomalyCounts,
-                },
-            });
+            // navigate("/certificatedata", {
+            //     state: {
+            //         ...d,
+            //         anomaly_counts: anomalyCounts,
+            //     },
+            // });
 
         };
 
